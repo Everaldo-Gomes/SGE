a77d2a71abd3ee1a08af6fdb837f1f36faccfa81
f-boschetti <felipeboss232@gmail.com>
2021-08-31 00:09:54 -0300

    Fim do dia

--- src/pages/moradores/listar/lista.php
+++ src/pages/moradores/listar/lista.php
@@ -1,18 +1,29 @@
 <?php
-$title = 'SGC - Lista de moradores';
-include_once("../../template/top.php");
-include_once("../../routers.php");
-include_once("../moradores/buscaMoradores");
+$title = 'SGC - Lista';
+
+include_once("../../../routers.php");
+include_once("../../../template/top.php");
+include_once("../../../database/funcoes_gerais.php");
+include_once("../../../database/conexao.php");
+
+$database = new Database();
+$db = $database->getConnection();
+
+$funcoesBanco = new Funcoes_gerais($db);
+
+$moradores = $funcoesBanco-> lerRegistrosMoradoresAtivosInativos('morador', false);
+
 ?>
+
+
 
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"> -->
 
 <div class="container">
-    <div class="row">
-        <div class="col-sm-12">
+    <div class="mt-5 row">
+        <div class="mt-5 col-sm-12">
         <table class="table">
             <thead>
                 <tr>
@@ -27,12 +38,12 @@ include_once("../moradores/buscaMoradores");
             <tbody>
                 <?php foreach($moradores as $moradorRegistrado){ ?>
                     <tr>
-                        <td><?= $moradorRegistrado['id']; ?></td>
-                        <td><?= $moradorRegistrado['nome']; ?></td>
-                        <td><?= $moradorRegistrado['telefone']; ?></td>
-                        <td><?= $moradorRegistrado['endereco']; ?></td>
+                        <td><?= $moradorRegistrado['0']; ?></td>
+                        <td><?= $moradorRegistrado['1']; ?></td>
+                        <td><?= $moradorRegistrado['3']; ?></td>
+                        <td><?= $moradorRegistrado['4']; ?></td>
                         <td><a href="#" class="btn btn-warning" role="button">Editar</span></a></td>
-                        <td><a href="deletaMorador.php?id=<?=$moradorRegistrado['id']?>" class="btn btn-danger" role="button">Remover</span></a></a></td>
+                        <td><a href="deletaMorador.php?id=<?=$moradorRegistrado['0']?>" class="btn btn-danger" role="button">Remover</span></a></a></td>
                     </tr>
                 <?php } ?>
             </tbody>
