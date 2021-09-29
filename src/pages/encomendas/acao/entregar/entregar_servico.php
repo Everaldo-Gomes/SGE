d95e78c112c2b35b6ebd49d6e2a2fb23e0bdf426
root <everaldo332@gmail.com>
2021-09-28 22:23:56 -0300

    inicio confirmação entrega

--- src/pages/encomendas/acao/entregar/entregar_servico.php
+++ src/pages/encomendas/acao/entregar/entregar_servico.php
@@ -26,11 +26,48 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 		// confirmação front e back FALTA FAZER 
 		$_SESSION['op_status'] = 1;
+
+        // gerando a confirmação de entrega (simulando que o morado que espera a enconmenda marcou que a entrega foi entregue)
+
+        // marca a entrega como entregue   DESCOMENTAR QUANDO O PDF ESTIVER OK
+        //$entregar_obj->alteraRegistroGeral("UPDATE encomenda set foi_entregue = 1 WHERE id = {$_SESSION['encomenda_id']}");
+
+
+        // obtendo algumas info
+        //morador, entrega, recebedor (igual aa tela de ação entregar e receber juntas)
+        
+        
+        // gerando o comprovante (PDF)
+         require('../../../../fpdf/fpdf.php');
+         $pdf = new FPDF('P','mm','B3');
+         
+         $pdf->AddPage();
+
+         $pdf->SetFont('Arial','B',30);
+         $pdf->Cell(100,1,'');
+         $pdf->Cell(10,1,'SGE');
+         
+         $pdf->SetFont('Arial','B',15);
+         $pdf->Cell(1,20,'Sistema de Gerenciamento de Encomendas');
+         
+         // $pdf->Cell(1,30,'Compravante de Entrega');
+         
+         // $pdf->SetFont('Arial','B',10);
+         // $pdf->Cell(1,40,'Gerando em ');
+
+         // $pdf->SetFont('Arial','B',8);
+         // $pdf->Cell(10,20,'Entregador: ');
+         // $pdf->Cell(10,20,'Recebedor: ');
+         // $pdf->Cell(10,20,'Entrega: ');
+         
+         // $pdf->Cell(10,30,'Código da entrega');
+         // $pdf->Cell(10,30,'gerar o código aqui');
+         $pdf->Output();
 	}
 
     /* volta para o inicio */
-    header("Location: " . $index_path);
-    exit();
+    //header("Location: " . $index_path);
+    //exit();
 }
 
 /* se não, carrega as informações normalmente */
