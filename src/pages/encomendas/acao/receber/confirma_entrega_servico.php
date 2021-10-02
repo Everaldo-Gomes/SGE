2f92c4d8ea6d898d8fe5d495c1958be25c6012fc
root <everaldo332@gmail.com>
2021-10-02 10:18:05 -0300

    Tarefa ID #3 OK

--- src/pages/encomendas/acao/receber/confirma_entrega_servico.php
+++ src/pages/encomendas/acao/receber/confirma_entrega_servico.php
@@ -6,9 +6,7 @@ include_once "../../../../routers.php";
 
 $database = new Database();
 $db = $database->getConnection();
-
 $entregar_obj = new funcoes_gerais($db);
-
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	
@@ -26,9 +24,17 @@ $entregador_info = $entregar_obj->lerRegistros("morador", "WHERE id = {$encomend
 $entregar_obj->alteraRegistroGeral("UPDATE encomenda set foi_entregue = 1 WHERE id = {$encomenda_id}");
 
 // salva histórico de entrega
+
+// Gerando o  código da entrega
+// formato: //ET = entrega + códgio gerado + BR = brasil
+$cod_entrega = "ET";
+$aux = $entregar_obj->selectRegistroGeral("SELECT MAX(id) FROM encomenda");
+$cod_entrega .= $aux[0][0];
+$cod_entrega .= "BR";
+
 $current_date = date('Y-m-d H:i:s');
-$query = "INSERT INTO historico_entrega (morador_entrega_id, morador_recebe_id, encomenda_id, data_entrega) 
-          VALUES ({$entregador_info[0]}, {$destinatario_info[0]}, {$entregador_info[0]}, '{$current_date }')";
+$query = "INSERT INTO historico_entrega (morador_entrega_id, morador_recebe_id, encomenda_id, data_entrega, cod_entrega) 
+          VALUES ({$entregador_info[0]}, {$destinatario_info[0]}, {$entregador_info[0]}, '{$current_date }', '{$cod_entrega}')";
 
 $entregar_obj->inserirRegistroGeral($query);
 
@@ -37,6 +43,6 @@ session_start();
 
 $_SESSION['encomenda_entregue'] = 1;
 
-header("Location: " . $editar_encomenda_path);
-exit();
+//header("Location: " . $editar_encomenda_path);
+//exit();
 ?>
--- src/pages/encomendas/acao/receber/gera_comprovante_servico.php
+++ src/pages/encomendas/acao/receber/gera_comprovante_servico.php
@@ -3,7 +3,7 @@
 include_once "../../../../database/conexao.php";
 include_once "../../../../database/funcoes_gerais.php";
 include_once "../../../../routers.php";
-require('../../../../fpdf/fpdf.php'); // biblioteca para gerar o comprovante (PDF)
+require('../../../../fpdf/fpdf.php'); // biblioteca para gerar o PDF
 
 $database = new Database();
 $db = $database->getConnection();
@@ -18,26 +18,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-// FAZER !!!
-// ================================================================================
-// MUDAR A CONSULTA PARA UM JOIN E EXIBIR AS INFO APARTIR DA TABELA historico_entrega
-// 
+// obtém info do recebedor, entregador, encomenda a partir do histórico de entrega
+$comprovante_info = array();
 
+$query = "SELECT 
+              encomenda.nome AS encomenda_nome, historico.data_entrega AS encomenda_data_entrega, 
+              recebedor.nome AS recebedor_nome, recebedor.telefone AS recebedor_telefone, recebedor.endereco AS recebedor_endereco, 
+              entregador.nome AS entregador_nome, entregador.telefone AS entregador_telefone, entregador.endereco AS entregador_endereco,
+              historico.cod_entrega
+          FROM encomenda encomenda 
+          JOIN historico_entrega historico ON encomenda.id = historico.encomenda_id 
+          JOIN morador recebedor ON historico.morador_recebe_id = recebedor.id 
+          JOIN morador entregador ON historico.morador_entrega_id = entregador.id";
 
-// obtendo algumas info do morador, entrega, recebedor para colocar no comprovante
-
-$encomenda_info = array();
-$destinatario_info = array();
-$entregador_info = array();
-
-$encomenda_info = $entregar_obj->lerRegistros("encomenda", "WHERE id = {$encomenda_id }");
-$destinatario_info = $entregar_obj->lerRegistros("morador", "WHERE id = {$encomenda_info[2]}");
-$entregador_info = $entregar_obj->lerRegistros("morador", "WHERE id = {$encomenda_info[3]}");
-
+$comprovante_info = $entregar_obj->selectRegistroGeral($query);
 
 // Gerando o PDF
 // config a página  array(largura x altura) em milímetro (mm)
-$pdf = new FPDF('P','mm', array(80,200));
+$pdf = new FPDF('P','mm', array(80,205));
 $pdf->AddPage();
 
 // config título
@@ -52,49 +50,46 @@ $pdf->Cell(-20,20,'Compravante de Entrega');
 // config corpo
 // entregador
 $pdf->SetFont('Arial','B',9);
-$pdf->Cell(5,40,'Sobre o entregador');
+$pdf->Cell(5,40,'Entregador');
          
 $pdf->SetFont('Arial','',8);
 
 $pdf->Cell(10,50,'Nome:');
-$pdf->Cell(-10,50, $entregador_info[1]);
+$pdf->Cell(-10,50, $comprovante_info[0][5]);
 
 $pdf->Cell(15,60,'Telefone:');
-$pdf->Cell(-15,60, $entregador_info[3]);
+$pdf->Cell(-15,60, $comprovante_info[0][6]);
 
 $pdf->Cell(15,69,'Endereco:');
-$pdf->Cell(-20,70, $entregador_info[4]);
+$pdf->Cell(-20,70, $comprovante_info[0][7]);
          
 // recebedor
 $pdf->SetFont('Arial','B',9);
-$pdf->Cell(5,90,'Sobre o recebedor');
+$pdf->Cell(5,90,'Recebedor');
 
 $pdf->SetFont('Arial','',8);
 
 $pdf->Cell(10,100,'Nome: ');
-$pdf->Cell(-10,100, $destinatario_info[1]);
+$pdf->Cell(-10,100, $comprovante_info[0][2]);
 
 $pdf->Cell(15,110,'Telefone: ');
-$pdf->Cell(-15,110, $destinatario_info[3]);
+$pdf->Cell(-15,110, $comprovante_info[0][3]);
 
 $pdf->Cell(15,120,'Endereco: ');
-$pdf->Cell(1,120, $destinatario_info[4]);
+$pdf->Cell(-20,120, $comprovante_info[0][4]);
          
 //encomenda
 $pdf->SetFont('Arial','B',9);
-$pdf->Cell(7,150,'Codigo da entrega');
+$pdf->Cell(5,140,'Encomenda');
 
 $pdf->SetFont('Arial','',8);
-         
-// Gerando o  código da entrega
-// formato: //ET = entrega + códgio gerado + RBR = realizada brasil
-$cod = "ET";
-$aux = $entregar_obj->selectRegistroGeral("SELECT MAX(id) FROM encomenda");
-$cod .= $aux[0][0];
-$cod .= "RBR";
-         
-$pdf->Cell(-7,160, $cod);
-$pdf->Cell(10,167, date('Y-m-d H:i:s'));
+
+$pdf->Cell(10,150,'Nome: ');
+$pdf->Cell(-10,150, $comprovante_info[0][0]);
+$pdf->Cell(25,160,'Data de Entrega: ');
+$pdf->Cell(-25,160, $comprovante_info[0][1]);
+$pdf->Cell(25,170,'Codigo da entrega: ');
+$pdf->Cell(1,170, $comprovante_info[0][8]);
 
 // mostra pdf
 $pdf->Output();
